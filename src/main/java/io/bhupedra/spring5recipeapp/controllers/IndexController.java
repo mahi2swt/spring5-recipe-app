72f90e161573130de8ced6b5b01d8b69844c9bb0
bhupendra <bhupendrapt95@gmail.com>
2020-04-14 12:40:13 +0530

    added Notes and Recipe Entity

--- src/main/java/io/bhupedra/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/io/bhupedra/spring5recipeapp/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndex(){
+        System.out.println("HEy yooo/...");
         return "index";
     }
-}
+    }
