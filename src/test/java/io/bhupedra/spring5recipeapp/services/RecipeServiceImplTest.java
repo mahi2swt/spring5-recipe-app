ca03c464a5716bac7fda53e68e38d055a63b439a
bhupendra <bhupendrapt95@gmail.com>
2020-04-17 19:31:52 +0530

    Performed JUnit and Mock test on IndexController

---
+++ src/test/java/io/bhupedra/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package io.bhupedra.spring5recipeapp.services;
+
+import io.bhupedra.spring5recipeapp.domain.Recipe;
+import io.bhupedra.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        //Testing if recipe set count is equal to actual count
+        assertEquals(recipes.size(), 1);
+
+        //verifying if recipeRepository is called only once
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
